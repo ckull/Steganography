81265c761c46f9bcaeff6d506e72962af3ffe438
dol162 <34243183+dol162@users.noreply.github.com>
2017-12-05 22:43:06 +0700

    Update Image.java

--- src/Image.java
+++ src/Image.java
@@ -8,16 +8,23 @@ import javax.imageio.ImageIO;
 import javax.swing.JOptionPane;
 
 
-
+/**
+ *
+ * This class use for do everything which concern with the image such as duplicate, read image file, transform pixel
+ * of the image into array byte.
+ *
+ * Created by Kullapat Siribodhi 58070503404
+ *            Thanadol Nimitchuchai 58070503442, 20 Nov 2017
+ * **/
 public class Image
 {
-	/*
-	 *Returns the complete path of a file, in the form: path\name.ext
-	 *@param path   The path (folder) of the file
-	 *@param name The name of the file
-	 *@param ext	  The extension of the file
-	 *@return A String representing the complete path of a file
-	 */
+	/**
+	 * This class use for return the complete form of extension in the form: path\name.ext
+	 * @param path : The folder or address of the file
+	 * @param name : The name of the file
+	 * @param ext : The extension of the file
+	 * @return return the string that can represent path of file
+	 **/
 	public static String image_path(String path, String name, String ext)
 	{
 		return path + "/" + name + "." + ext;
@@ -25,13 +32,15 @@ public class Image
 	
 	
 	
-	/*
-	 *Set method to save an image file
-	 *@param image The image file to save
-	 *@param file	  File  to save the image to
-	 *@param ext	  The extension and thus format of the file to be saved
-	 *@return Returns true if the save is succesful
-	 */
+	/**
+	 * 
+	 *This method use for save an image file that will be the encoded image file
+	 * @param image : The image file that we need to save
+	 * @param file : The File that use for save the image
+	 * @param ext : The extension and format of the file that have saved
+	 * @return return boolean value for tell the process succeeds or not
+	 * 
+	 **/
 	public static boolean setImage(BufferedImage image, File file, String ext)
 	{
 		try
@@ -48,14 +57,13 @@ public class Image
 		}
 	}
 	
-	/*
-	 *Gets the byte array of an image
-	 *@param image The image to get byte data from
-	 *@return Returns the byte array of the image supplied
-	 *@see Raster
-	 *@see WritableRaster
-	 *@see DataBufferByte
-	 */
+	/**
+	 * This class use for transform the pixel in the image into byte array
+	 *
+	 * @param image : The image that need to change into byte array data
+	 * @return return the byte array of the image
+	 *
+	 **/
 	public static byte[] get_byte_data(BufferedImage image)
 	{
 		WritableRaster raster   = image.getRaster();
@@ -63,11 +71,14 @@ public class Image
 		return buffer.getData();
 	}
 	
-	/*
-	 *Creates a user space version of a Buffered Image, for editing and saving bytes
-	 *@param image The image to put into user space, removes compression interferences
-	 *@return The user space version of the supplied image
-	 */
+	/**
+	 *  This class use to create the imitate version of Buffered image for void any effect that influence
+	 * to the original picture when editing and saving bytes
+	 *
+	 * @param image : The original image that we choose from device to put into imitate version
+	 * @return The imitate version of the supplied image
+	 *
+	 **/
 	public static BufferedImage duplicateImage(BufferedImage image)
 	{
 		//create new_img with the attributes of image
@@ -78,12 +89,14 @@ public class Image
 		return new_img;
 	}
 	
-	/*
-	 *Get method to return an image file
-	 *@param f The complete path name of the image.
-	 *@return A BufferedImage of the supplied file path
-	 *@see	Steganography.image_path
-	 */
+	/**
+	 * This method use for return image that has correct form of extension and have
+	 * completed path name
+	 *
+	 * @param f : The complete path name of the image.
+	 * @return return a BufferedImage that already has correct file path and extension
+	 *
+	 **/
 	public static BufferedImage getImage(String f)
 	{
 		BufferedImage 	image	= null;
