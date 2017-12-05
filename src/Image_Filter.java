5ca8cb9a9264f6ca5265bf81d0aeb53c622ea5f8
dol162 <34243183+dol162@users.noreply.github.com>
2017-12-05 22:54:18 +0700

    Update Image_Filter.java

--- src/Image_Filter.java
+++ src/Image_Filter.java
@@ -2,7 +2,15 @@
 
 import java.io.*; 
 
-
+/**
+ *
+ * This class use for get extension from the input file and check it is the correct extension that appropriate for
+ * our program or not.
+ * The extensions that our program can import into encode processing are jpeg and png
+ *
+ * Created by Kullapat Siribodhi 58070503404
+ *            Thanadol Nimitchuchai 58070503442, 20 Nov 2017
+ * **/
 public class Image_Filter extends javax.swing.filechooser.FileFilter {
 	
     public boolean accept(File f) {
@@ -29,12 +37,13 @@ public class Image_Filter extends javax.swing.filechooser.FileFilter {
         return "Supported Image files";
     }
     
-    /*
-	 *Determines the Extension
-	 *@param f File to return the extension of
-	 *@return Returns the String representing the extension
-	 */
-	protected static String getExtension(File f)
+    /**
+     * This method use for define the extension of input file
+     *
+     * @param f is the file that need to return the extension
+     * @return return string that can represent the extension
+     * **/
+     protected static String getExtension(File f)
 	{
 		String s = f.getName();
 		int i = s.lastIndexOf('.');
